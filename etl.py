2b1bd18892e7a324459b6ff59fdf25f7a3c79000
Mateo Dennehy <mateodennehy@Mateos-MacBook-Air.local>
2021-12-21 14:17:37 -0300

    please work

--- etl.py
+++ etl.py
@@ -1,6 +1,6 @@
 import requests
 import json
-import os
+from os.path import join
 from datetime import datetime
 
 
@@ -16,6 +16,7 @@ def get_miner_data(miner_address):
 if __name__ == '__main__':
     now = datetime.now()
     data = get_miner_data(miner_address="fc8f035129c537fc19e504613b841fd12aa035e7")
+    folder = "/home/matedennehy/Antartica/antartica-etl"
     name = f"{now.year}-{now.month}-{now.day}_{now.hour}:{now.minute}_api_data.json"
-    with open(name, 'w') as f:
-        json.dump(data, os.path.join("/home/matedennehy/Antartica/antartica-etl", f), indent = 2)
+    with open(join(folder, name), 'w') as f:
+        json.dump(data, f, indent = 2)
