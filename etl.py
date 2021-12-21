2d37c6d8eb174ac5c01ad6d824a8be2dcaddac07
Mateo Dennehy <mateodennehy@Mateos-MacBook-Air.local>
2021-12-21 14:11:19 -0300

    ..

--- etl.py
+++ etl.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import os
 from datetime import datetime
 
 
@@ -17,4 +18,4 @@ if __name__ == '__main__':
     data = get_miner_data(miner_address="fc8f035129c537fc19e504613b841fd12aa035e7")
     name = f"{now.year}-{now.month}-{now.day}_{now.hour}:{now.minute}_api_data.json"
     with open(name, 'w') as f:
-        json.dump(data, f, indent = 2)
+        json.dump(data, os.join("/home/matedennehy/Antartica/antartica-etl", f), indent = 2)
